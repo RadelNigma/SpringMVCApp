0187fe99b273d13877c7491daaed9ccaf6a29d23
RadelNigma <radelnigma@yandex.ru>
2022-03-23 11:21:26 +0300

    task calculator

--- src/main/java/ru/nigma/springcourse/config/controllers/FirstController.java
+++ src/main/java/ru/nigma/springcourse/config/controllers/FirstController.java
@@ -1,14 +1,23 @@
 package ru.nigma.springcourse.config.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("/first")
 public class FirstController {
     @GetMapping("/hello")
-    public String helloPage(){
+    public String helloPage(@RequestParam(value = "name", required = false) String name,
+                            @RequestParam(value = "surname", required = false) String surname,
+                             Model model){
+
+       // System.out.println("Привет " + name + " " + surname);
+        model.addAttribute("message","Hello " + name + " " + surname);
         return "first/hello";
     }
     @GetMapping("/goodbye")
