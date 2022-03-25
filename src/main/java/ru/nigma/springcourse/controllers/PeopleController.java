cf9d8209439cd941492850d2bbc2a23f46ff4f41
RadelNigma <radelnigma@yandex.ru>
2022-03-25 20:46:03 +0300

    Validate

--- src/main/java/ru/nigma/springcourse/controllers/PeopleController.java
+++ src/main/java/ru/nigma/springcourse/controllers/PeopleController.java
@@ -1,8 +1,10 @@
 package ru.nigma.springcourse.controllers;
 
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import ru.nigma.springcourse.dao.PersonDAO;
 import ru.nigma.springcourse.models.Person;
@@ -33,7 +35,12 @@ model.addAttribute("person",personDAO.show(id));
         return "people/new";
     }
     @PostMapping
-    public String create(@ModelAttribute("person") Person person){
+    public String create(@ModelAttribute("person") @Valid Person person,
+                         BindingResult bindingResult){
+
+        if (bindingResult.hasErrors()){
+            return "people/new";
+        }
         personDAO.save(person);
         return "redirect:/people";
     }
@@ -45,7 +52,11 @@ model.addAttribute("person",personDAO.show(id));
     }
 
     @PatchMapping("/{id}")
-    public String update(@ModelAttribute("person") Person person, @PathVariable("id") int id){
+    public String update(@ModelAttribute("person") @Valid Person person,
+                         BindingResult bindingResult, @PathVariable("id") int id){
+        if(bindingResult.hasErrors()){
+            return "people/edit";
+        }
         personDAO.update(id, person);
         return "redirect:/people";
     }
